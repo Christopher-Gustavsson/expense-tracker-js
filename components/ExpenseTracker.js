7fe1346b5de730152b73136ee18ef09e39a8a088
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-05-24 22:04:10 -0700

    HTML elements are being passed to expense tracker component started working on functions: addCLickHandlers, addBill, cancelBill, clearInputs:

--- components/ExpenseTracker.js
+++ components/ExpenseTracker.js
@@ -1,20 +1,44 @@
 
 class ExpenseTracker{
     constructor(elementConfig){
-        this.elementConfig = elementConfig;
+        this.elementConfig = {
+            addButton: elementConfig.addButton,
+            cancelButton: elementConfig.cancelButton,
+            vendorInput: elementConfig.vendorInput,
+            descriptionInput: elementConfig.descriptionInput,
+            amountInput: elementConfig.amountInput,
+            dueDateInput: elementConfig.dueDateInput
+        };
+
         this.data = {};
 
         this.addBill = this.addBill.bind(this);
+        this.cancelBill = this.cancelBill.bind(this);
     }
 
-    addEventListeners(){
-        this.elementConfig.addButton.addEventListeners('click', addBill);
+    addClickHandlers(){
+        this.elementConfig.addButton.addEventListener('click', this.addBill);
+        this.elementConfig.cancelButton.addEventListener('click', this.cancelBill);
+
     }
 
     addBill(){
-        let vendorName = this.elementConfig.vendorName;
-        console.log("hi");
-        console.log(vendorName);
+        let vendorName = this.elementConfig.vendorInput.value;
+        let descriptionInput = this.elementConfig.descriptionInput.value;
+        let amountInput = this.elementConfig.amountInput.value;
+        let dueDateInput = this.elementConfig.dueDateInput.value;
+        this.clearInputs();
+    }
+
+    cancelBill(){
+        this.clearInputs();
+    }
+
+    clearInputs(){
+        this.elementConfig.vendorInput.value = '';
+        this.elementConfig.descriptionInput.value = '';
+        this.elementConfig.amountInput.value= '';
+        this.elementConfig.dueDateInput.value = '';
     }
 }
 
--- main.js
+++ main.js
@@ -3,12 +3,13 @@ document.addEventListener("DOMContentLoaded", startApp);
 function startApp(){
     const expenseTracker = new ExpenseTracker({
         addButton: document.getElementById('add-button'),
-        closeButton: document.getElementById('cancel-button'),
+        cancelButton: document.getElementById('cancel-button'),
         vendorInput: document.getElementById('vendor'),
         descriptionInput: document.getElementById('description'),
         amountInput: document.getElementById('amount'),
         dueDateInput: document.getElementById('due-date')
     });
-    console.log(expenseTracker);
+
+   expenseTracker.addClickHandlers();
 }
 
